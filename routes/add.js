14e1d57c9fefb4f24e96b2b5597c89af8e0d8107
Peter-White <pwhite007@icloud.com>
2015-06-26 19:29:11 -0400

    Prepare for patch

--- routes/add.js
+++ routes/add.js
@@ -1,9 +1,29 @@
 var passport = require('passport');
 var express = require('express');
 var router = express.Router();
+var Item = require('./../lib/items.js');
 
-router.get('/', function(req, res) {
-  res.render('add', {});
-});
+router.route('/')
+  .post(function(req, res, next) {
+    console.log(req.body);
+    Item.register({
+        itemName: req.body.username,
+        description: req.body.firstName,
+        price: req.body.lastName,
+        photoURL: req.body.phoneNumber,
+        tags: req.body.emailAddress
+      },
+      function(err, item) {
+        if (err) {
+          return res.render('add', {
+            item: item
+          });
+        }
+
+        req.login(user, function(err) {
+          res.redirect('/');
+        });
+      })
+  })
 
 module.exports = router;
