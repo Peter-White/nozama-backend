8254911a68cfd11ca7bf5a2ef3de4908eff9d930
Peter-White <pwhite007@icloud.com>
2015-06-28 10:39:43 -0400

    Still Doesn't Work

--- routes/add.js
+++ routes/add.js
@@ -1,33 +1,33 @@
 var passport = require('passport');
 var express = require('express');
 var router = express.Router();
-var Item = require('./../lib/items.js');
+
+var Item = require('../lib/items.js');
 
 router.route('/')
-router.get('/', function(req, res) {
-  res.render('add', {});
-})
+  .get(function(req, res, next) {
+    res.render('add', {});
+  })
   .post(function(req, res, next) {
     console.log(req.body);
-    Item.register({
-        itemName: req.body.username,
-        description: req.body.firstName,
-        price: req.body.lastName,
-        photoURL: req.body.phoneNumber,
-        tags: req.body.emailAddress
+    Item.add({
+        itemName: req.body.itemName,
+        description: req.body.description,
+        price: req.body.price,
+        photoURL: req.body.photoURL,
+        tags: req.body.tags
       },
+
       function(err, item) {
         if (err) {
           return res.render('add', {
             item: item
           });
         }
-        req.login(user, function(err) {
+        req.login(item, function(err) {
           res.redirect('/');
         });
       })
   })
-
-
 
 module.exports = router;
