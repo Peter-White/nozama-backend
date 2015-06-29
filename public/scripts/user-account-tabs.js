3ab35d4344717d84e3204c0bb2a0bd599fd92860
amygoldberg <amykgoldberg@gmail.com>
2015-06-29 10:25:46 -0400

    tabs for logged in user working (cart and order history tabs still empty at this point)

--- public/scripts/user-account-tabs.js
+++ public/scripts/user-account-tabs.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
-  $('#tab-1').show();
-  $('#tab-2').hide();
-  $('#tab-3').hide();
-  $(document).on('click', '#tab-2', function() {
-    $('.maincontent').html();
-    $('#tab-2').addClass('active');
-    $('#tab-1').removeClass('active');
-    $('#tab-2').show();
-    $('#tab-1').hide();
-  });
+  // $(document).on('click', '#tabsy a', function(e) {
+  // e.preventDefault();
+  // $(this).tab('show');
+  // });
 });
