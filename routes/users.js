ffba98e1834d0e094d1662669ed6b1ec2c1b6fa0
azitowolf <azito.wolf3@gmail.com>
2015-06-30 13:15:09 -0400

    connected stripe checkout btn

--- routes/users.js
+++ routes/users.js
@@ -95,7 +95,6 @@ router.get('/:id', function(req, res) {
       })
     })
 
-
     setTimeout(function() {
       res.render('user', {
         user: req.user,
@@ -103,6 +102,9 @@ router.get('/:id', function(req, res) {
         total: total,
         cartCount: req.session.cart.products.length
       });
+      console.log('total is' + total)
+      req.session.cart.total = total;
+      console.log('TOTAL OF CART' + req.session.cart.total);
     }, 1000)
 
   } else {
